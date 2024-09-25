91e2a4dc90c3a06af46ff3fe665a84f24c475e04
Ayush Chakraborty <chakraborty.ayush.1998@gmail.com>
2024-09-25 21:14:58 +0530

    added api to fetch word data, and added command wise data printing

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const commandHandler = new CommandHandler();
 
 async function run() {
     const args = process.argv.slice(2); // Get command line arguments
-    console.log(args);
-    commandHandler.commandsArray = args;
-    commandHandler.getWordInformation();
+    commandHandler.handleCommand(args);
 
     //console.log(await apiClient.getWordInformation());
 }
--- service/CommandHandler.js
+++ service/CommandHandler.js
@@ -3,12 +3,74 @@ const ApiClient = require('./ApiClient');
 const apiClient = new ApiClient();
 
 class CommandHandler {
-    commandsArray;
 
-    constructor(commandsArray) {
-        this.commandsArray = commandsArray;
+    constructor() {
+        
+    }
 
-        console.log(this.commandsArray);
+    async handleCommand(commandArgs) {
+        let command = commandArgs[0];
+        let word = commandArgs[1];
+
+        const wordData = await apiClient.getWordInformation(word);
+
+        let resultDataArray = [];
+
+        switch (command) {
+            case 'def':
+                this.printCommandData(this.getDefinitions(wordData, resultDataArray), 'Definitions', word)
+                break;
+            case 'syn':
+                this.printCommandData(this.getSynonyms(wordData, resultDataArray), 'Synonyms', word)
+                break;
+            case 'ant':
+                this.printCommandData(this.getAntonyms(wordData, resultDataArray), 'Antonyms', word)
+                break;
+            case 'ex':
+                this.printCommandData(this.getExamples(wordData, resultDataArray), 'Examples', word)
+                break;
+            case 'full':
+                this.printCommandData(this.getDefinitions(wordData, resultDataArray), 'Definitions', word)
+                this.printCommandData(this.getSynonyms(wordData, resultDataArray), 'Synonyms', word)
+                this.printCommandData(this.getAntonyms(wordData, resultDataArray), 'Antonyms', word)
+                this.printCommandData(this.getExamples(wordData, resultDataArray), 'Examples', word)
+                break;
+            default:
+                console.log('Unknown command:', command);
+        }
+
+    }
+
+    printCommandData(resultDataArray, commandName, word) {
+        console.log('\n');
+        console.log(`${commandName} for the word "${word}" are:`);
+        resultDataArray.forEach((text, index)=> console.log(`${index+1}. ${text}`));
+        return;
+    }
+
+    getDefinitions(wordData, resultDataArray) {
+        wordData.forEach(element => {
+            element.meanings.flatMap(meaning => meaning.definitions.map(def => def.definition && resultDataArray.push(def.definition)));
+        });
+        return resultDataArray;
+    }
+    getExamples(wordData, resultDataArray) {
+        wordData.forEach(element => {
+            element.meanings.flatMap(meaning => meaning.definitions.map(def => def.example && resultDataArray.push(def.example)));
+        });
+        return resultDataArray;
+    }
+    getAntonyms(wordData, resultDataArray) {
+        wordData.forEach(element => {
+            element.meanings.flatMap(meaning => meaning.antonyms && resultDataArray.push(...meaning.antonyms));
+        });
+        return resultDataArray;
+    }
+    getSynonyms(wordData, resultDataArray) {
+        wordData.forEach(element => {
+            element.meanings.flatMap(meaning => meaning.synonyms && resultDataArray.push(...meaning.synonyms));
+        });
+        return resultDataArray;
     }
 
     async getWordInformation() {
