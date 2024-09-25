9750efdcc2524a70cbedcbec79a457a2ba4fa9e0
Ayush Chakraborty <chakraborty.ayush.1998@gmail.com>
2024-09-25 19:19:50 +0530

    basic get call on user input

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello!");
+const CommandHandler = require('./service/CommandHandler');
+
+const commandHandler = new CommandHandler();
+
+async function run() {
+    const args = process.argv.slice(2); // Get command line arguments
+    console.log(args);
+    commandHandler.commandsArray = args;
+    commandHandler.getWordInformation();
+
+    //console.log(await apiClient.getWordInformation());
+}
+
+run();
