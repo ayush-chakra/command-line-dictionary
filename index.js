ae7dc4681ae183df89cc49b5ee8a5ee10dde56e3
Ayush Chakraborty <chakraborty.ayush.1998@gmail.com>
2024-09-26 14:16:15 +0530

    added WordGame

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const commandHandler = new CommandHandler();
 async function run() {
     const args = process.argv.slice(2); // Get command line arguments
     commandHandler.handleCommand(args);
-
-    //console.log(await apiClient.getWordInformation());
 }
 
 run();
--- service/ApiClient.js
+++ service/ApiClient.js
@@ -5,12 +5,22 @@ class ApiClient {
     constructor(word) {
         this.word = word;
     }
+
     async getWordInformation(word) {
         try {
             const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
             return response.data;
         } catch (error) {
-            console.error(error);
+            throw new Error('No definition found, please try again!');
+        }
+    }
+
+    async getRandomWord() {
+        try {
+            const response = await axios.get('https://random-word-api.vercel.app/api?words=1');
+            return response.data[0];
+        } catch (error) {
+           throw new Error('Failed to fetch a random word!');
         }
     }
 }
